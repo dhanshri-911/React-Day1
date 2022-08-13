3dc8b2084f1c6ac4544636b91f85f395af3dd0bf
Dhanshri911 <dszingade911@gmail.com>
2022-08-13 23:07:41 +0530

    Added:UC5 User Input OnNameChange

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './assets/BridgeLabz.jpg'
-import logo1 from './assets/1img.webp'
-import logo2 from './assets/2img.jpg'
-import logo3 from './assets/3img.webp'
+// import logo1 from './assets/1img.webp'
+// import logo2 from './assets/2img.jpg'
+// import logo3 from './assets/3img.webp'
 import React from 'react';
 import './App.css';
 
@@ -9,8 +9,11 @@ class App extends React.Component{
  url = 'http://www.bridgelabz.com/'
   constructor(){
     super()
+    //  this.state = {
+    //    title: 'hello from bridgelabz' 
+    //  }
     this.state = {
-      title: 'hello from bridgelabz' 
+      userName: ''
     }
   }
 
@@ -19,21 +22,29 @@ class App extends React.Component{
     console.log("save button is clicked!",$event);
     window.open(this.url, "blank");
   }
-  
+  //onChange Function
+  onNameChange = (event) => {
+    console.log("value is",event.target.value);
+    this.setState({ userName: event.target.value})
+  }
 
-render(){
+render() {
   return(
+    <>
     <div>
     <h1>Hello Bridgelabz</h1>
      <h1>{this.state.title}</h1>
+     <h1>Hello {this.state.userName} from Bridgelabz</h1>
      <img src={logo} onClick={this.onClick}
      alt="The Bridgelabz logo: a bridge tp Employement throgh lab works"/>
      {/* <img src={logo1} />
      <img src={logo2}/>
-     <img  src={logo3}/>
-     
-      */}
+     <img  src={logo3}/> */}
+     </div> 
+    <div>
+    <input onChange = {this.onNameChange} />
     </div>
+    </>
   );
 }
 }
