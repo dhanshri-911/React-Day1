e568c6bfd16bec1091e4f4ee63554b5c24d7a825
Dhanshri911 <dszingade911@gmail.com>
2022-08-13 22:16:05 +0530

    Added:UC3 Propery Binding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-//import logo from './logo.svg';
+import logo from './assets/BridgeLabz.jpg'
+import logo1 from './assets/1img.webp'
+import logo2 from './assets/2img.jpg'
+import logo3 from './assets/3img.webp'
 import React from 'react';
 import './App.css';
 
@@ -16,6 +19,13 @@ render(){
     <div>
     <h1>Hello Bridgelabz</h1>
      <h1>{this.state.title}</h1>
+     <img src={logo}
+     alt="The Bridgelabz logo: a bridge tp Employement throgh lab works"/>
+     {/* <img src={logo1} />
+     <img src={logo2}/>
+     <img  src={logo3}/>
+     
+      */}
     </div>
   );
 }
